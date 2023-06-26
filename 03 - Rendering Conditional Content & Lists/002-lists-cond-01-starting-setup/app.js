40df757cfc470df51e6b42a07ab20178641d35d8
Daniel <delmontejohndaniel1415@gmail.com>
2023-06-26 16:00:13 +0800

    removing list item

--- 03 - Rendering Conditional Content & Lists/002-lists-cond-01-starting-setup/app.js
+++ 03 - Rendering Conditional Content & Lists/002-lists-cond-01-starting-setup/app.js
@@ -13,8 +13,11 @@ const app = Vue.createApp({
     clearEnteredGoalValue() {
       this.enteredGoalValue = '';
     },
-    removeGoal() {
-
+    // Removing an item to the list of goals
+    // The main point is that, whenever you want to manipulate state or the content of the page, always look for the model of the Vue app and not in the template
+    // The template will only serve as the template and the state and model are being handle by Vue app
+    removeGoal(idx) {
+      this.goals.splice(idx, 1);
     }
   }
 });
