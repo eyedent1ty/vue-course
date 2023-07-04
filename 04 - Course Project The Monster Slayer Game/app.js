d4b3ded78e4bcf43eea30027554a71740afad7cb
Daniel <delmontejohndaniel1415@gmail.com>
2023-07-04 13:48:35 +0800

    implemented the battle log between player and monster

--- 04 - Course Project The Monster Slayer Game/app.js
+++ 04 - Course Project The Monster Slayer Game/app.js
@@ -9,7 +9,8 @@ const app = Vue.createApp({
       playerHealth: 100,
       monsterHealth: 100,
       currentRound: 0,
-      winner: null
+      winner: null,
+      logMessages: []
     };
   },
   watch: {
@@ -49,21 +50,25 @@ const app = Vue.createApp({
       this.monsterHealth = 100;
       this.currentRound = 0;
       this.winner = null;
+      this.logMessages = [];
     },
     attackMonster() {
       this.currentRound++;
       const attackValue = getRandomValue(5, 12);
       this.monsterHealth -= attackValue;
+      this.addLogMessage('player', 'attack', attackValue);
       this.attackPlayer();
     },
     attackPlayer() {
       const attackValue = getRandomValue(5, 15);
       this.playerHealth -= attackValue;
+      this.addLogMessage('monster', 'attack', attackValue);
     },
     specialAttackMonster() {
       this.currentRound++;
       const attackValue = getRandomValue(10, 25);
       this.monsterHealth -= attackValue;
+      this.addLogMessage('player', 'attack', attackValue);
       this.attackPlayer();
     },
     healPlayer() {
@@ -74,12 +79,20 @@ const app = Vue.createApp({
       } else {
         this.playerHealth += healthValue;
       }
+      this.addLogMessage('player', 'heal', healthValue);
       this.attackPlayer();
     },
     surrender() {
       this.winner = 'monster';
+    },
+    addLogMessage(who, what, value) {
+      this.logMessages.unshift({
+        actionBy: who,
+        actionType: what,
+        actionValue: value
+      });
     }
-  }
+  },
 });
 
 app.mount('#game');
