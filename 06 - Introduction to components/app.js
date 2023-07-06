c64d99f5a8738aa1b91de294481e06391f579a8a
Daniel <delmontejohndaniel1415@gmail.com>
2023-07-06 14:43:43 +0800

    one of the benefits of component

--- 06 - Introduction to components/app.js
+++ 06 - Introduction to components/app.js
@@ -1,34 +1,56 @@
 const app = Vue.createApp({
   data() {
     return {
-      detailsAreVisible: false,
       friends: [
         {
           id: 'manuel',
           name: 'Manuel Lorenz',
           phone: '01234 5678 991',
           email: 'manuel@localhost.com',
-          detailsAreVisible: false
         },
         {
           id: 'julie',
           name: 'Julie Jones',
           phone: '09876 543 221',
           email: 'julie@localhost.com',
-          detailsAreVisible: false
         }
       ]
     };
+  }
+});
+
+app.component('friend-contact', {
+  template: `
+    <li>
+      <h2>{{ friend.name }}</h2>
+      <button @click="toggleDetails()">{{ actionMessage }}</button>
+      <ul v-if="detailsAreVisible">
+        <li><strong>Phone:</strong> {{ friend.phone }}</li>
+        <li><strong>Email:</strong> {{ friend.email }}</li>
+      </ul>
+    </li>
+  `,
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: 'manuel',
+        name: 'Manuel Lorenz',
+        phone: '01234 5678 991',
+        email: 'manuel@localhost.com',
+      }
+    };
   },
   methods: {
-    toggleDetails(id) {
-      const friend = this.friends.find((currentFriend) => {
-        return currentFriend.id === id;
-      });
-
-      friend.detailsAreVisible = !friend.detailsAreVisible;
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
     }
-  } 
+  },
+  computed: {
+    actionMessage() {
+      return `${this.detailsAreVisible ? 'Hide' : 'Show'} Details`;
+    }
+  }
 });
 
 app.mount('#app');
