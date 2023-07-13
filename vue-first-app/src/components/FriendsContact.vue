9e292a6b522a43d629936600a254fbe4701f4d3e
Daniel <delmontejohndaniel1415@gmail.com>
2023-07-13 16:19:06 +0800

    using the vue cli created project to create vue app

---
+++ vue-first-app/src/components/FriendsContact.vue
@@ -0,0 +1,31 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">{{ isShowDetailsActive ? 'Hide' : 'Show' }} Details</button>
+    <ul v-show="isShowDetailsActive">
+      <li><strong>Phone:</strong> {{ friend.phone }}</li>
+      <li><strong>Email:</strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friend: {
+        id: 'manuel',
+        name: 'Manuel Lopez',
+        phone: '0123 4567 90',
+        email: 'manuel@localhost.com'
+      },
+      isShowDetailsActive: false
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.isShowDetailsActive = !this.isShowDetailsActive;
+    }
+  }
+};
+</script>
