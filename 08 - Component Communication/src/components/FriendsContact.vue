26f58a16e504ccd4e9d9106de30244927c4d482b
Daniel <delmontejohndaniel1415@gmail.com>
2023-07-14 11:47:21 +0800

    another way of defining props of the component

--- 08 - Component Communication/src/components/FriendsContact.vue
+++ 08 - Component Communication/src/components/FriendsContact.vue
@@ -1,8 +1,10 @@
 <template>
   <li>
-    <h2>{{ name }} {{ isFriendFavorite === '1' ? '(Favorite)' : '' }}</h2>
+    <h2>{{ name }} {{ isFriendFavorite === "1" ? "(Favorite)" : "" }}</h2>
     <button @click="toggleFavorite">Toggle favorite</button>
-    <button @click="toggleDetails">{{ isShowDetailsActive ? 'Hide' : 'Show' }} Details</button>
+    <button @click="toggleDetails">
+      {{ isShowDetailsActive ? "Hide" : "Show" }} Details
+    </button>
     <ul v-show="isShowDetailsActive">
       <li><strong>Phone:</strong> {{ phoneNumber }}</li>
       <li><strong>Email:</strong> {{ emailAddress }}</li>
@@ -12,22 +14,35 @@
 
 <script>
 export default {
-  props: [
-    'name',
-    'phoneNumber',
-    'emailAddress',
-    'isFavorite'
-  ],
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: String,
+    emailAddress: {
+      type: String,
+      required: true,
+    },
+    isFavorite: {
+      type: String,
+      required: false,
+      default: "0",
+      validator(value) {
+        return value === "1" || value === "0";
+      },
+    },
+  },
   data() {
     return {
       friend: {
-        id: 'manuel',
-        name: 'Manuel Lopez',
-        phone: '0123 4567 90',
-        email: 'manuel@localhost.com'
+        id: "manuel",
+        name: "Manuel Lopez",
+        phone: "0123 4567 90",
+        email: "manuel@localhost.com",
       },
       isShowDetailsActive: false,
-      isFriendFavorite: this.isFavorite
+      isFriendFavorite: this.isFavorite,
     };
   },
   methods: {
@@ -43,12 +58,17 @@ export default {
     //   }
     // }
     toggleFavorite() {
-      if (this.isFriendFavorite === '0') {
-        this.isFriendFavorite = '1';
+      if (this.isFriendFavorite === "0") {
+        this.isFriendFavorite = "1";
       } else {
-        this.isFriendFavorite = '0';
+        this.isFriendFavorite = "0";
       }
-    }
-  }
+    },
+  },
+  // The props are converted as properties of the component instance, same level with the data properties and methods
+  mounted() {
+    // Display the proxy object of the component instance
+    console.log(this);
+  },
 };
 </script>
