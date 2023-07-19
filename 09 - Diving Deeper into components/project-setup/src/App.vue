2c12ac25511bd12a72e30ec3ced7f90d4a972fd2
Daniel <delmontejohndaniel1415@gmail.com>
2023-07-19 23:34:42 +0800

    making a component dynamic using component element

--- 09 - Diving Deeper into components/project-setup/src/App.vue
+++ 09 - Diving Deeper into components/project-setup/src/App.vue
@@ -1,43 +1,31 @@
 <template>
   <the-header></the-header>
-  <badge-list></badge-list>
-  <user-info
-    :full-name="activeUser.name"
-    :info-text="activeUser.description"
-    :role="activeUser.role"
-  ></user-info>
-  <course-goals>
-    <template #default="slotProps">
-      <h2>{{ slotProps.item }}</h2>
-      <p>{{ slotProps.another }}</p>
-    </template>
-  </course-goals>
+  <button @click="setSelectedComponent('active-goals')">Active Goals</button>
+  <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
+  <component :is="selectedComponent"></component>
 </template>
 
 <script>
 import TheHeader from "./components/TheHeader.vue";
-import BadgeList from "./components/BadgeList.vue";
-import UserInfo from "./components/UserInfo.vue";
-import CourseGoals from "./components/CourseGoals.vue";
+import ActiveGoals from './components/ActiveGoals.vue';
+import ManageGoals from './components/ManageGoals.vue';
 
 export default {
-  // Vue automatically convert these components into kebab case
-  // Local component
   components: {
     TheHeader,
-    BadgeList,
-    UserInfo,
-    CourseGoals,
+    ActiveGoals,
+    ManageGoals
   },
   data() {
     return {
-      activeUser: {
-        name: "Maximilian Schwarzmüller",
-        description: "Site owner and admin",
-        role: "admin",
-      },
+      selectedComponent: 'active-goals'
     };
   },
+  methods: {
+    setSelectedComponent(cmp) {
+      this.selectedComponent = cmp;
+    }
+  }
 };
 </script>
 
