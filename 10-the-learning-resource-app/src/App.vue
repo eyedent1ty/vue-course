60616937a1ef007eba68ae2c0830f11cc670bb5e
Daniel <delmontejohndaniel1415@gmail.com>
2023-07-26 08:53:07 +0800

    first step for building the app

---
+++ 10-the-learning-resource-app/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <ul>
+    <stored-resource v-for="storedResource in storedResources" :item="storedResource" :key="storedResource.id"></stored-resource>
+  </ul>
+</template>
+
+<script>
+import StoredResource from './components/StoredResource.vue';
+
+export default {
+  components: {
+    StoredResource
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation.',
+          link: 'https://vuejs.org'
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.com'
+        }
+      ]
+    };
+  }
+}
+</script>
