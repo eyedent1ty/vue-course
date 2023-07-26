e9d044aaf8d4836a90f411b8cc3bf5c3281d17d4
Daniel <delmontejohndaniel1415@gmail.com>
2023-07-26 09:20:44 +0800

    Created a component that is used for rendering learning resources and implemented the delete functionality

---
+++ 10-the-learning-resource-app/src/components/learning-resources/LearningResource.vue
@@ -0,0 +1,26 @@
+<template>
+  <li>
+    <div>
+      <header>
+        <h3>{{ title }}</h3>
+        <button @click="emitDeleteEvent">Delete</button>
+      </header>
+    </div>
+    <p>{{ description }}</p>
+    <nav>
+      <a :href="link">View Resource</a>
+    </nav>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ['id', 'title', 'description', 'link'],
+  emits: ['delete-resource'],
+  methods: {
+    emitDeleteEvent() {
+      this.$emit('delete', this.id);
+    }
+  }
+};
+</script>
