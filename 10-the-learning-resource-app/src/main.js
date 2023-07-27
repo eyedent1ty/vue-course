41bd12bb656ee1f5a95d822efef43a407712d16c
Daniel <delmontejohndaniel1415@gmail.com>
2023-07-27 15:40:07 +0800

    created header and base-card components

--- 10-the-learning-resource-app/src/main.js
+++ 10-the-learning-resource-app/src/main.js
@@ -1,8 +1,11 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import BaseCard from './components/UI/BaseCard.vue';
 
-    import './assets/global.css';
+import './assets/global.css';
 
 const app = createApp(App);
+
+app.component('base-card', BaseCard)
 
 app.mount('#app');
