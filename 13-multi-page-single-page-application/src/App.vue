5e7a54a570463233d04cf765b444d60e6dc5cc4c
Daniel <delmontejohndaniel1415@gmail.com>
2023-08-05 21:54:53 +0800

    added navigation using router links

--- 13-multi-page-single-page-application/src/App.vue
+++ 13-multi-page-single-page-application/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <the-navigation @set-page="setActivePage"></the-navigation>
+  <the-navigation></the-navigation>
   <main>
     <router-view></router-view>
   </main>
@@ -34,12 +34,7 @@ export default {
       teams: this.teams,
       users: this.users,
     };
-  },
-  methods: {
-    setActivePage(page) {
-      this.activePage = page;
-    },
-  },
+  }
 };
 </script>
 
