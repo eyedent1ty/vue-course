a0afa3889aa25b63a9b05d5197f3d53c60432436
Daniel <delmontejohndaniel1415@gmail.com>
2023-08-08 16:02:19 +0800

    using the utility classes from transition component with the animation css property

--- 14-animations/src/App.vue
+++ 14-animations/src/App.vue
@@ -92,7 +92,7 @@ button:active {
   animation: slide-fade 2s ease-in infinite alternate;
 }
 
-@keyframes slide-fade {
+@keyframes slide-scale {
   0% {
     transform: translate(0, 0);
   }
@@ -107,30 +107,32 @@ button:active {
 }
 
 .v-enter-from {
-  opacity: 0;
-  transform: translateY(-30px);
+  /* opacity: 0;
+  transform: translateY(-30px); */
 }
 
 .v-enter-active {
-  transition: all 0.3s ease-out;
+  /* transition: all 0.3s ease-out; */
+  animation: slide-scale 0.3s ease-in;
 }
 
 .v-enter-to {
-  opacity: 1;
-  transform: translateY(0);
+  /* opacity: 1;
+  transform: translateY(0); */
 }
 
 .v-leave-from {
-  opacity: 1;
-  transform: translateY(0);
+  /* opacity: 1;
+  transform: translateY(0); */
 }
 
 .v-leave-active {
-  transition: all 0.3s ease-out;
+  /* transition: all 0.3s ease-out; */
+  animation: slide-scale 0.3s ease-out;
 }
 
 .v-leave-to {
-  opacity: 0;
-  transform: translateY(30px);
+  /* opacity: 0;
+  transform: translateY(30px); */
 }
 </style>
