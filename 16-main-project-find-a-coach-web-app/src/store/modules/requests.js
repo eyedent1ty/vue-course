ba49dc51c11ed05e252c94c09a5ee51be220aec0
Daniel <delmontejohndaniel1415@gmail.com>
2023-08-15 14:32:25 +0800

    outputting requests

--- 16-main-project-find-a-coach-web-app/src/store/modules/requests.js
+++ 16-main-project-find-a-coach-web-app/src/store/modules/requests.js
@@ -25,6 +25,9 @@ export default {
   getters: {
     requests(state) {
       return state.requests;
+    },
+    hasRequests(state) {
+      return state.requests && state.requests.length > 0;
     }
   }
 };
