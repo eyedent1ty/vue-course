8c22edfa0976871e107d40ac7e233b8558dd3d6b
Daniel <delmontejohndaniel1415@gmail.com>
2023-08-18 10:31:48 +0800

    getting all the users stored in firebase

--- 16-main-project-find-a-coach-web-app/src/pages/CoachesList.vue
+++ 16-main-project-find-a-coach-web-app/src/pages/CoachesList.vue
@@ -5,7 +5,7 @@
   <section>
     <base-card>
       <div class="controls">
-        <base-button mode="outline">Refresh</base-button>
+        <base-button mode="outline" @click="loadCoaches">Refresh</base-button>
         <base-button link to="/register" v-if="!isCoach"
           >Register a Coach</base-button
         >
@@ -74,7 +74,13 @@ export default {
   methods: {
     setFilters(updatedFilters) {
       this.activeFilters = updatedFilters;
+    },
+    loadCoaches() {
+      this.$store.dispatch('coaches/loadCoaches');
     }
+  },
+  created() { 
+    this.loadCoaches();
   }
 };
 </script>
--- 16-main-project-find-a-coach-web-app/src/store/modules/coaches.js
+++ 16-main-project-find-a-coach-web-app/src/store/modules/coaches.js
@@ -3,30 +3,33 @@ export default {
   state() {
     return {
       coaches: [
-        {
-          id: 'c1',
-          firstName: 'Maximilian',
-          lastName: 'Schwarzmüller',
-          areas: ['frontend', 'backend', 'career'],
-          description:
-            "I'm Maximilian and I've worked as a freelance web developer for years. Let me help you become a developer as well!",
-          hourlyRate: 30
-        },
-        {
-          id: 'c2',
-          firstName: 'Julie',
-          lastName: 'Jones',
-          areas: ['frontend', 'career'],
-          description:
-            'I am Julie and as a senior developer in a big tech company, I can help you get your first job or progress in your current role.',
-          hourlyRate: 30
-        }
+        // {
+        //   id: 'c1',
+        //   firstName: 'Maximilian',
+        //   lastName: 'Schwarzmüller',
+        //   areas: ['frontend', 'backend', 'career'],
+        //   description:
+        //     "I'm Maximilian and I've worked as a freelance web developer for years. Let me help you become a developer as well!",
+        //   hourlyRate: 30
+        // },
+        // {
+        //   id: 'c2',
+        //   firstName: 'Julie',
+        //   lastName: 'Jones',
+        //   areas: ['frontend', 'career'],
+        //   description:
+        //     'I am Julie and as a senior developer in a big tech company, I can help you get your first job or progress in your current role.',
+        //   hourlyRate: 30
+        // }
       ]
     };
   },
   mutations: {
-    registerCoach(state, payload) {
-      state.coaches.push(payload);
+    registerCoach(state, newCoach) {
+      state.coaches.push(newCoach);
+    },
+    setCoaches(state, newCoaches) {
+      state.coaches = newCoaches;
     }
   },
   actions: {
@@ -49,6 +52,28 @@ export default {
         ...coachData,
         id: userId
       });
+    },
+    async loadCoaches(context) {
+      const response = await fetch(
+        'https://find-a-coach-project-fd7f4-default-rtdb.firebaseio.com/coaches.json'
+      );
+
+      if (!response.ok) {
+        return;
+      }
+
+      const data = await response.json();
+
+      const coaches = [];
+
+      Object.keys(data).forEach((id) => {
+        coaches.push({
+          id,
+          ...data[id]
+        });
+      });
+
+      context.commit('setCoaches', coaches);
     }
   },
   getters: {
