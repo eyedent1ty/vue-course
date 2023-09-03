70ff8bce360cdc5fc6056807554ca27090154687
Daniel <delmontejohndaniel1415@gmail.com>
2023-09-04 00:06:21 +0800

    added hook for alert

--- 21-understanding-hooks-or-custom-composition-functions-or-composable/src/components/AddUser.vue
+++ 21-understanding-hooks-or-custom-composition-functions-or-composable/src/components/AddUser.vue
@@ -11,32 +11,24 @@
 </template>
 
 <script>
-import { ref } from 'vue';
-
 import UserAlert from './UserAlert.vue';
+
+import useAlert from '@/hooks/alert.js';
 
 export default {
   components: {
     UserAlert
   },
   setup() {
-    const alertIsVisible = ref(false);
-
-    function showAlert() {
-      alertIsVisible.value = true;
-    } 
-
-    function hideAlert() {
-      alertIsVisible.value = false;
-    }
+    const [alertIsVisible, showAlert, hideAlert] = useAlert();
 
     return {
       alertIsVisible,
       showAlert,
       hideAlert
-    }
+    };
   }
-}
+};
 </script>
 
 <style scoped>
--- 21-understanding-hooks-or-custom-composition-functions-or-composable/src/components/DeleteUser.vue
+++ 21-understanding-hooks-or-custom-composition-functions-or-composable/src/components/DeleteUser.vue
@@ -15,30 +15,22 @@
 </template>
 
 <script>
-import { ref } from 'vue';
-
 import UserAlert from './UserAlert.vue';
+
+import useAlert from '@/hooks/alert';
 
 export default {
   components: {
     UserAlert
   },
   setup() {
-    const alertIsVisible = ref(false);
-
-    function showAlert() {
-      alertIsVisible.value = true;
-    } 
-
-    function hideAlert() {
-      alertIsVisible.value = false;
-    }
+    const [alertIsVisible, showAlert, hideAlert] = useAlert();
 
     return {
       alertIsVisible,
       showAlert,
       hideAlert
-    }
+    };
   }
 }
 </script>
