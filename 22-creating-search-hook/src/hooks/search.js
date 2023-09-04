9767e01b2402453733f05efb5244ed9677d80f81
Daniel <delmontejohndaniel1415@gmail.com>
2023-09-04 12:38:09 +0800

    succesfully convert the code from UserList and ProjectList search functionality to hook

---
+++ 22-creating-search-hook/src/hooks/search.js
@@ -0,0 +1,35 @@
+import { ref, computed, watch } from 'vue';
+
+export default function useSearch(items, searchProp) {
+  const enteredSearchTerm = ref('');
+  const activeSearchTerm = ref('');
+
+
+  console.log('Search hook is executed');
+  const availableItems = computed(function () {
+    if (activeSearchTerm.value) {
+      return items.value.filter((item) =>
+        item[searchProp].includes(activeSearchTerm.value)
+      );
+    }
+    return items.value;
+  });
+
+  watch(enteredSearchTerm, function (newValue) {
+    setTimeout(() => {
+      if (newValue === enteredSearchTerm.value) {
+        activeSearchTerm.value = newValue;
+      }
+    }, 300);
+  });
+
+  function updateSearch(val) {
+    enteredSearchTerm.value = val;
+  }
+
+  return {
+    enteredSearchTerm,
+    availableItems,
+    updateSearch,
+  };
+}
